1c44554338149352ffe67b2fe8d66864d9fc9b73
Radosław Bucki <236507@edu.p.lodz.pl>
2023-03-04 22:05:07 +0100

    WordNumberExtractor

--- src/main/java/backend/extractor/ExtractorType.java
+++ src/main/java/backend/extractor/ExtractorType.java
@@ -1,15 +1,13 @@
 package backend.extractor;
 
-import backend.extractor.extractors.ArticleLengthExtractor;
-import backend.extractor.extractors.MostUsedWordExtractor;
-import backend.extractor.extractors.DaysFromCreationDateExtractor;
-import backend.extractor.extractors.SentenceNumberExtractor;
+import backend.extractor.extractors.*;
 
 public enum ExtractorType {
     ARTICLE_LENGTH(new ArticleLengthExtractor()),
     MOST_USED_WORD(new MostUsedWordExtractor()),
     DAYS_FROM_CREATION_DATE(new DaysFromCreationDateExtractor()),
-    SENTENCE_NUMBER(new SentenceNumberExtractor());
+    SENTENCE_NUMBER(new SentenceNumberExtractor()),
+    WORD_NUMBER(new WordNumberExtractor());
 
     private final Extractor<?> extractor;
 
---
+++ src/main/java/backend/extractor/extractors/WordNumberExtractor.java
@@ -0,0 +1,15 @@
+package backend.extractor.extractors;
+
+import backend.extractor.Extractor;
+import backend.model.Article;
+
+import java.util.Optional;
+
+public class WordNumberExtractor implements Extractor<Integer> {
+    @Override
+    public Integer extract(Article article) {
+        return Optional.ofNullable(article.getText().getText())
+                .map(text -> text.split("\\s+").length)
+                .orElse(0);
+    }
+}
