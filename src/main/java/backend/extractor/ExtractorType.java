2fd4a919ec5b2a2779db0eaac1ca2da35915d2fc
Radosław Bucki <236507@edu.p.lodz.pl>
2023-03-04 22:11:00 +0100

    UniqueWordsNumberExtractor

--- src/main/java/backend/extractor/ExtractorType.java
+++ src/main/java/backend/extractor/ExtractorType.java
@@ -7,7 +7,8 @@ public enum ExtractorType {
     MOST_USED_WORD(new MostUsedWordExtractor()),
     DAYS_FROM_CREATION_DATE(new DaysFromCreationDateExtractor()),
     SENTENCE_NUMBER(new SentenceNumberExtractor()),
-    WORD_NUMBER(new WordNumberExtractor());
+    WORD_NUMBER(new WordNumberExtractor()),
+    UNIQUE_WORDS_NUMBER(new UniqueWordsNumberExtractor());
 
     private final Extractor<?> extractor;
 
