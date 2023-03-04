225fd0f044f5f6529a3f68408ab2031f2400757d
Radosław Bucki <236507@edu.p.lodz.pl>
2023-03-04 21:54:09 +0100

    SentenceNumberExtractor

--- src/main/java/backend/extractor/ExtractorType.java
+++ src/main/java/backend/extractor/ExtractorType.java
@@ -3,11 +3,13 @@ package backend.extractor;
 import backend.extractor.extractors.ArticleLengthExtractor;
 import backend.extractor.extractors.MostUsedWordExtractor;
 import backend.extractor.extractors.DaysFromCreationDateExtractor;
+import backend.extractor.extractors.SentenceNumberExtractor;
 
 public enum ExtractorType {
     ARTICLE_LENGTH(new ArticleLengthExtractor()),
     MOST_USED_WORD(new MostUsedWordExtractor()),
-    DAYS_FROM_CREATION_DATE(new DaysFromCreationDateExtractor());
+    DAYS_FROM_CREATION_DATE(new DaysFromCreationDateExtractor()),
+    SENTENCE_NUMBER(new SentenceNumberExtractor());
 
     private final Extractor<?> extractor;
 
