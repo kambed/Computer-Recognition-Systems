6d9952139a8f0fd8a6c309145675f74b9a4955d9
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-05 12:20:48 +0100

    Word average length extractor

---
+++ src/main/java/backend/extractor/extractors/WordAverageLengthExtractor.java
@@ -0,0 +1,23 @@
+package backend.extractor.extractors;
+
+import backend.extractor.Extractor;
+import backend.model.Article;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class WordAverageLengthExtractor implements Extractor<Double> {
+
+    @Override
+    public Double extract(Article article) {
+        return Optional.ofNullable(
+                        article.getText().getText()
+                ).map(
+                        text -> text.split("\\s+")
+                ).map(
+                        words -> Arrays.stream(words)
+                                .mapToInt(String::length)
+                                .average().orElse(0.0)
+                ).orElse(0.0);
+    }
+}
