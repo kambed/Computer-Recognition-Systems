0278f46bc27cadd51fa34e108f612da16cad9e70
Radosław Bucki <236507@edu.p.lodz.pl>
2023-03-10 21:30:14 +0100

    Merge issue

--- src/main/java/backend/extractor/ExtractorType.java
+++ src/main/java/backend/extractor/ExtractorType.java
@@ -10,7 +10,6 @@ public enum ExtractorType {
     MOST_USED_YEAR(new MostUsedYearExtractor()),
     MOST_USED_WORD_STARTING_IN_CAPITAL_LETTER(new MostUsedWordStartingInCapitalLetterExtractor()),
     CITY_FROM_DATELINE(new CityFromDatelineExtractor()),
-    MOST_USED_GEOGRAPHICAL_NAME_MAPPED_TO_COUNTRY(new MostUsedGeographicalNameMappedToCountryExtractor()),
     MOST_USED_COUNTRY_NAME(new MostUsedCountryNameExtractor()),
     MOST_USED_COUNTRY_NAME_IN_TITLE(new MostUsedCountryNameInTitleExtractor()),
     PEOPLE_COUNTRY(new PeopleCountryExtractor()),
