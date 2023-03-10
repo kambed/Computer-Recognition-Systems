5b8179f46ac4365f8b8b2c8a25ecf3ce54b63447
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-10 15:55:43 +0100

    People countries extractor

---
+++ src/main/java/backend/extractor/extractors/PeopleCountryExtractor.java
@@ -0,0 +1,41 @@
+package backend.extractor.extractors;
+
+import backend.extractor.Extractor;
+import backend.model.Article;
+import backend.reader.CsvReader;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PeopleCountryExtractor implements Extractor<String> {
+
+    @Override
+    public String extract(Article article) {
+        Map<String, List<String>> peopleCountry = peopleCountries();
+        return Optional.ofNullable(article.getPeople()).orElse(List.of())
+                .stream()
+                .filter(person -> peopleCountry.values().stream().flatMap(Collection::stream).toList().contains(person))
+                .map(person -> {
+                    for (Map.Entry<String, List<String>> entry : peopleCountry.entrySet()) {
+                        if (entry.getValue().contains(person)) {
+                            return entry.getKey();
+                        }
+                    }
+                    return "";
+                }).filter(word -> !word.isEmpty())
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        LinkedHashMap::new,
+                        Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(entry -> entry.getKey().toLowerCase())
+                .orElse("");
+    }
+
+    private Map<String, List<String>> peopleCountries() {
+        return CsvReader.readDictionary("src/main/resources/backend/people.csv").orElseThrow();
+    }
+}
