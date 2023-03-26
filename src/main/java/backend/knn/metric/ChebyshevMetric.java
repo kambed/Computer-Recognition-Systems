0b9f14a566616be0bc13f75b0304d2c2619aa0a6
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-26 11:33:22 +0200

    Metrics skeleton + Manhattan metric implemented

---
+++ src/main/java/backend/knn/metric/ChebyshevMetric.java
@@ -0,0 +1,10 @@
+package backend.knn.metric;
+
+import java.util.List;
+
+public class ChebyshevMetric implements Metric {
+    @Override
+    public double calculateDistance(List<Double> vector1, List<Double> vector2) {
+        return 0;
+    }
+}
