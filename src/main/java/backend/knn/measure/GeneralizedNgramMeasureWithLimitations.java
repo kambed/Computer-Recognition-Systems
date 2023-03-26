b36e3b18d5a513ad1043115de1299cf9f0016a3a
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-26 13:36:29 +0200

    PR comments

--- src/main/java/backend/knn/measure/GeneralizedNgramMeasureWithLimitations.java
+++ src/main/java/backend/knn/measure/GeneralizedNgramMeasureWithLimitations.java
@@ -4,27 +4,27 @@ import java.util.stream.IntStream;
 
 public class GeneralizedNgramMeasureWithLimitations implements Measure {
 
-    int shortestNgram;
-    int longestNgram;
+    private final int shortestGram;
+    private final int longestGram;
 
-    public GeneralizedNgramMeasureWithLimitations(int shortestNgram, int longestNgram) {
-        this.shortestNgram = shortestNgram;
-        this.longestNgram = longestNgram;
+    public GeneralizedNgramMeasureWithLimitations(int shortestGram, int longestGram) {
+        this.shortestGram = shortestGram;
+        this.longestGram = longestGram;
     }
 
     @Override
     public double calculateMeasure(String text1, String text2) {
         int N = Math.max(text1.length(), text2.length());
-        String text1LowerCase = text1.toLowerCase();
-        String text2LowerCase = text2.toLowerCase();
-        int commonNgrams = Math.min(countCommonNgrams(text1LowerCase, text2LowerCase),
-                countCommonNgrams(text2LowerCase, text1LowerCase));
+        String text1Preformatted = text1.toLowerCase().trim();
+        String text2Preformatted = text2.toLowerCase().trim();
+        int commonNgrams = Math.min(countCommonNgrams(text1Preformatted, text2Preformatted),
+                countCommonNgrams(text2Preformatted, text1Preformatted));
         return commonNgrams * (2.0 /
-                (((N - shortestNgram + 1) * (N - shortestNgram + 2)) - ((N - longestNgram + 1) * (N - longestNgram))));
+                (((N - shortestGram + 1) * (N - shortestGram + 2)) - ((N - longestGram + 1) * (N - longestGram))));
     }
 
     private int countCommonNgrams(String text1, String text2) {
-        return IntStream.range(shortestNgram, longestNgram + 1).map(n ->
+        return IntStream.range(shortestGram, longestGram + 1).map(n ->
                 IntStream.range(0, text1.length() - n + 1).map(text1Index ->
                         text2.contains(text1.substring(text1Index, text1Index + n)) ? 1 : 0
                 ).sum()
--- src/main/java/backend/knn/measure/MeasureFactory.java
+++ src/main/java/backend/knn/measure/MeasureFactory.java
@@ -1,6 +1,10 @@
 package backend.knn.measure;
 
 public class MeasureFactory {
+
+    private MeasureFactory() {
+    }
+
     public static Measure createGeneralizedNgramWithLimitations(int shortestGram, int longestGram) {
         return new GeneralizedNgramMeasureWithLimitations(shortestGram, longestGram);
     }
