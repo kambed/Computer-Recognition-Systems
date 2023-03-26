ad95204137496a655edcf31788af040201c0f444
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-03-26 11:36:27 +0200

    Chebyshev metric implemented

--- src/main/java/backend/knn/metric/ChebyshevMetric.java
+++ src/main/java/backend/knn/metric/ChebyshevMetric.java
@@ -1,10 +1,17 @@
 package backend.knn.metric;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class ChebyshevMetric implements Metric {
     @Override
     public double calculateDistance(List<Double> vector1, List<Double> vector2) {
-        return 0;
+        if (vector1.size() != vector2.size()) {
+            return Double.MAX_VALUE;
+        }
+
+        return IntStream.range(0, vector1.size())
+                .mapToDouble(i -> Math.abs(vector1.get(i) - vector2.get(i)))
+                .max().orElseThrow();
     }
 }
