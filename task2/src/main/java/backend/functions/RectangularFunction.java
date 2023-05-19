f7461e22694767cbb47a111ae98b40d401485cbb
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-05-19 13:05:24 +0200

    Rectangular function

---
+++ task2/src/main/java/backend/functions/RectangularFunction.java
@@ -0,0 +1,7 @@
+package backend.functions;
+
+public class RectangularFunction extends TrapezoidalFunction {
+    public RectangularFunction(double minSupp, double maxSupp) {
+        super(minSupp, minSupp, maxSupp, maxSupp);
+    }
+}
