78cdf9be54cb6eea77fe1146218bd9924c8336b7
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-05-27 21:01:01 +0200

    Type 4 first

--- task2/src/main/java/backend/lingustic/summary/MultiType4Summary.java
+++ task2/src/main/java/backend/lingustic/summary/MultiType4Summary.java
@@ -4,16 +4,31 @@ import backend.lingustic.LabeledFuzzySet;
 import backend.lingustic.Subject;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MultiType4Summary extends MultiSubjectSummary {
-    protected MultiType4Summary(List<Subject> subjects, List<LabeledFuzzySet> summarizers, List<String> summarizerVariableNames, String summary) {
-        super(subjects, summarizers, summarizerVariableNames, summary);
+    public MultiType4Summary(List<Subject> subjects, List<LabeledFuzzySet> summarizers, List<String> summarizerVariableNames) {
+        super(subjects, summarizers, summarizerVariableNames, "Więcej " + subjects.get(0).getName() + " niż " + subjects.get(1).getName() +
+                " jest/ma " + summarizers.stream().map(LabeledFuzzySet::getLabel).collect(Collectors.joining(" i ")));
         this.t1 = evaluateT1();
         this.finalDegreeOfTruth = evaluateFinalDegreeOfTruth();
     }
 
     @Override
     protected double evaluateT1() {
-        return 0;
+        Map<Integer, Double> s1Card = subjects.get(0).getAllElementsCardinality(summarizers, summarizerVariableNames);
+        Map<Integer, Double> s2Card = subjects.get(1).getAllElementsCardinality(summarizers, summarizerVariableNames);
+        return 1 - IntStream.range(0, Math.max(s1Card.keySet().stream().max(Integer::compareTo).orElseThrow(),
+                        s2Card.keySet().stream().max(Integer::compareTo).orElseThrow())
+                )
+                .mapToDouble(i ->
+                        Math.min(1, 1 - s1Card.getOrDefault(i, 0.0) + s2Card.getOrDefault(i, 0.0))
+                )
+                .boxed()
+                .mapToDouble(Double::doubleValue)
+                .average()
+                .orElseThrow();
     }
 }
