da91432863211633b1e2029655e6fbfc15fccdb4
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-05-29 19:57:50 +0200

    Update quantifiers requirements

--- task2/src/main/java/backend/lingustic/quantifier/AbsoluteQuantifier.java
+++ task2/src/main/java/backend/lingustic/quantifier/AbsoluteQuantifier.java
@@ -1,9 +1,15 @@
 package backend.lingustic.quantifier;
 
+import backend.domain.ContinuousDomain;
 import backend.functions.BaseFunction;
+import backend.repository.StatsRepository;
 
 public class AbsoluteQuantifier extends AbstractQuantifier {
     public AbsoluteQuantifier(String label, BaseFunction function) {
         super(label, function);
+        function.setDomain(new ContinuousDomain(0, StatsRepository.getStats().size()));
+        if (!this.isConvex() || !this.isNormal()) {
+            throw new IllegalArgumentException("Quantifier is not convex and normal");
+        }
     }
 }
