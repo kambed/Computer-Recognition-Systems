a493f47bdd097584599fab2291182f5bdf37fa25
Kamil Bednarek <kamil.bednarek.2001@gmail.com>
2023-05-30 14:08:42 +0200

    Update quantifier constructors

--- task2/src/main/java/backend/lingustic/quantifier/AbstractQuantifier.java
+++ task2/src/main/java/backend/lingustic/quantifier/AbstractQuantifier.java
@@ -6,8 +6,5 @@ import backend.lingustic.LabeledFuzzySet;
 public abstract class AbstractQuantifier extends LabeledFuzzySet {
     protected AbstractQuantifier(String label, BaseFunction function) {
         super(label, function);
-        if (!this.isConvex() || !this.isNormal()) {
-            throw new IllegalArgumentException("Quantifier is not convex and normal");
-        }
     }
 }
